225ee0855e708cf16e59e94a7cfc16509cfac55e
Simon Bonnedahl <simbo803@student.liu.se>
2022-12-27 15:30:35 +0100

    Added icon pack and basic toolbar

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -3,9 +3,17 @@ import React from 'react'
 
 const Toolbar: React.FC = () => {
 
+const options = ["Size", "Color", "Text"]
   return (
    
-    <div className='w-44 h-auto border '>Toolbar</div>
+    <div className='flex flex-col w-44 space-y-4 items-center mt-4'>
+        {options.map(option =>(
+            <div className='text-center font-light text-lg'>
+                <div className='w-28 h-28 bg-white rounded-full shadow-2xl hover:scale-110 ease-in-out duration-300'></div>
+                {option}
+            </div>
+        ))}
+    </div>
   );
 };
 
