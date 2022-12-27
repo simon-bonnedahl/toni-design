59f09ea1e3b0e76a6c0939b55826dae724aa3a3e
Simon Bonnedahl <simbo803@student.liu.se>
2022-12-27 23:38:29 +0100

    cleanup

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signBoardReducer from "./reducers/signboardSlice";
 import toolbarReducer from "./reducers/toolbarSlice";
+
 export const store = configureStore({
   reducer: {
     signBoard: signBoardReducer,
