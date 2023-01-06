2feda24e8b33287b7e5e2bb273dc040e49ae6edd
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-06 21:56:50 +0100

    Deployment test

---
+++ pages/api/test.ts
@@ -0,0 +1,11 @@
+import { NextApiResponse, NextApiRequest } from "next";
+
+type Data = {
+  name: string;
+};
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
+}
