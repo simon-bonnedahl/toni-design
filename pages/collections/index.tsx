dd4d68e3416f7198940434eff11218a21ea4c35f
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-08 02:14:50 +0100

    Cleanup and bugfixes

--- pages/collections/index.tsx
+++ pages/collections/index.tsx
@@ -26,7 +26,7 @@ function Home() {
         <title>Checkout</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col bg-base-100 w-screen h-screen">
+      <main className="flex flex-col bg-base-100 w-screen">
         <HeaderBar />
         <Navbar />
         <div className="grid grid-cols-4 gap-4 p-4">
