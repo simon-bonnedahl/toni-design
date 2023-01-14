9531d6a784a54f24ea6b2dad387e66281d780264
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-14 16:05:20 +0100

    Updates to the account page

---
+++ pages/account/creations.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const Creations = () => {
+  useEffect(() => {
+    //fetch
+  }, []);
+  return (
+    <div>
+      <h1>Minas skapelser</h1>
+    </div>
+  );
+};
+
+export default Creations;
