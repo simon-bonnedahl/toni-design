ec8d8b568351f9e297f5f23375c43a64dce58b94
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-15 22:28:23 +0100

    NextAuth deployment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -34,8 +34,10 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  pages: {
+    signIn: "/login",
+  },
   callbacks: {
     async jwt({ token, user, account }) {
       if (account && user) {
