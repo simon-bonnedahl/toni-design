34b1c06e22dc74b10656228afe754af0c59243ed
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-15 15:06:56 +0100

    Theme fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,28 +11,6 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme2: {
-          primary: "#000000",
-
-          secondary: "#17c4aa",
-
-          accent: "#a736db",
-
-          neutral: "#1a1a1a",
-
-          "base-100": "#ffffff",
-          "base-200": "#f7e8d3",
-
-          info: "#3863eb",
-          "info-content": "#d1e0ff",
-
-          success: "#21a249",
-          "success-content": "#bdffd5",
-
-          warning: "#9D5407",
-
-          error: "#F3305D",
-        },
         light: {
           primary: "#000000",
 
