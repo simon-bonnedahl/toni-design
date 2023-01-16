c12cd678e72c96100e69351cb0e8be65859d2cd1
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-16 13:54:04 +0100

    NextAuth deployment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,57 +2,35 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import client from "../../../sanity";
 const passwordHash = require("password-hash");
+import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "toni-design",
-      credentials: {
-        email: {
-          label: "email",
-          type: "email",
-          placeholder: "jsmith@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        let query = `*[_type == "account" && email == $email][0]`;
-        let params = { email: credentials.email };
-        let account = await client.fetch(query, params);
-        if (account) {
-          if (passwordHash.verify(credentials.password, account.password)) {
-            return { email: account.email, name: account.firstname };
-          }
-        }
-        return null;
-      },
-    }),
-    // ...add more providers here
-  ],
-
-  secret: "test",
-  pages: {
-    signIn: "/login",
-  },
+export const authOptions = {
+  // Include user.id on session
   callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        return {
-          ...token,
-          accessToken: user.token,
-          refreshToken: user.refreshToken,
-        };
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
       }
-
-      return token;
-    },
-
-    async session({ session, token }) {
-      session.user.accessToken = token.accessToken;
-      session.user.refreshToken = token.refreshToken;
-      session.user.accessTokenExpires = token.accessTokenExpires;
-
       return session;
     },
   },
-});
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    /**
+     * ...add more providers here
+     *
+     * Most other providers require a bit more work than the Discord provider.
+     * For example, the GitHub provider requires you to add the
+     * `refresh_token_expires_in` field to the Account model. Refer to the
+     * NextAuth.js docs for the provider you want to use. Example:
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
+};
+
+export default NextAuth(authOptions);
